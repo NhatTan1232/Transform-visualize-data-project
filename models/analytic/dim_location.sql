a17d1bfd36b0096f7051ceeb64bf6e1391aa195e
NhatTan1232 <vovankhanh12345678@gmail.com>
2024-09-09 20:49:04 +0700

    Update continent_name

--- models/analytic/dim_location.sql
+++ models/analytic/dim_location.sql
@@ -9,6 +9,25 @@ WITH dim_location__cast_type AS
   FROM `de-gcp-project.glamira_stg.stg_dim_location`
 )
 
+,dim_location__add_continent AS
+(
+  SELECT
+    location_key
+    ,city_name
+    ,country_name
+    ,country_code
+    ,region_name
+    ,CASE
+      WHEN country_name IN ('Afghanistan', 'Armenia', 'Azerbaijan', 'Bahrain', 'Bangladesh', 'China', 'Georgia', 'Hong Kong', 'India', 'Indonesia', 'Iran (Islamic Republic of)', 'Iraq', 'Israel', 'Japan', 'Jordan', 'Kazakhstan', 'Korea (the Republic of)', 'Kuwait', 'Lebanon', 'Malaysia', 'Myanmar', 'Nepal', 'Oman', 'Pakistan', 'Palestine, State of', 'Philippines', 'Qatar', 'Saudi Arabia', 'Singapore', 'Sri Lanka', 'Syrian Arab Republic', 'Taiwan (Province of China)', 'Thailand', 'United Arab Emirates', 'Viet Nam') THEN 'Asia'
+      WHEN country_name IN ('Aland Islands', 'Albania', 'Andorra', 'Austria', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia', 'Denmark', 'Estonia', 'Faroe Islands', 'Finland', 'France', 'Germany', 'Gibraltar', 'Greece', 'Greenland', 'Guernsey', 'Hungary', 'Iceland', 'Ireland', 'Isle of Man', 'Italy', 'Jersey', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Malta', 'Monaco', 'Montenegro', 'Netherlands (Kingdom of the)', 'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russian Federation', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Ukraine', 'United Kingdom of Great Britain and Northern Ireland') THEN 'Europe'
+      WHEN country_name IN ('Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Cabo Verde', 'Cameroon', 'Congo', 'Congo (the Democratic Republic of the)', "Cote D'ivoire", 'Egypt', 'Gabon', 'Ghana', 'Kenya', 'Madagascar', 'Malawi', 'Mali', 'Mauritius', 'Mayotte', 'Morocco', 'Mozambique', 'Nigeria', 'Reunion', 'Rwanda', 'Sao Tome and Principe', 'Senegal', 'Somalia', 'South Africa', 'Tanzania, the United Republic of', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe') THEN 'Africa'
+      WHEN country_name IN ('Antigua and Barbuda', 'Argentina', 'Aruba', 'Bahamas (the)', 'Barbados', 'Belize', 'Bolivia (Plurinational State of)', 'Brazil', 'Canada', 'Chile', 'Colombia', 'Costa Rica', 'Cuba', 'Dominica', 'Dominican Republic', 'Ecuador', 'El Salvador', 'Grenada', 'Guadeloupe', 'Guatemala', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama', 'Paraguay', 'Peru', 'Puerto Rico', 'Saint Lucia', 'Trinidad and Tobago', 'United States of America', 'Uruguay', 'Venezuela (Bolivarian Republic of)', 'Virgin Islands (U.S.)') THEN 'North America'
+      WHEN country_name IN ('Australia', 'Fiji', 'French Polynesia', 'New Caledonia', 'New Zealand', 'Samoa') THEN 'Oceania'
+      ELSE 'undefined'
+    END AS continent_name 
+  FROM dim_location__cast_type
+)
+
 ,dim_location__add_null AS
 (
   SELECT
@@ -17,9 +36,10 @@ WITH dim_location__cast_type AS
     ,'undefined' AS country_name
     ,'undefined' AS country_code
     ,'undefined' AS region_name
+    ,'undefined' AS continent_name
   UNION ALL
   SELECT *
-  FROM dim_location__cast_type
+  FROM dim_location__add_continent
 )
 
 SELECT *
